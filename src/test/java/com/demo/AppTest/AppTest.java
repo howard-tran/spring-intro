6701f89244ae57139488d649853cab4da6e5d517
mingkhoi <mingkhoitran1234@gmail.com>
2020-09-29 01:03:20 +0700

    add controller-layer

--- src/test/java/com/demo/AppTest/AppTest.java
+++ src/test/java/com/demo/AppTest/AppTest.java
@@ -2,12 +2,13 @@ package com.demo.AppTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.demo.App.*;
 
-@SpringBootTest(classes = { App.class })
+@SpringBootTest(classes = App.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 class AppTest {
   @Test
   void contextLoads() {
